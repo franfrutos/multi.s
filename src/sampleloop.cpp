3910408dbf8eeaac205b51a26b6bd5d6951508a6
Francisco Garre Frutos <90706203+franfrutos@users.noreply.github.com>
2024-02-15 16:50:56 +0100

    Updated sampleloop.cpp

--- src/sampleloop.cpp
+++ src/sampleloop.cpp
@@ -1,22 +1,22 @@
 #include <Rcpp.h>
 
-// wrapper around R's RNG such that we get a uniform distribution over
+// wrapper around Rs RNG such that we get a uniform distribution over
 // [0,n) as required by the STL algorithm
 inline int randWrapper(const int n) { return floor(unif_rand()*n); }
 
-//' @export
+// @export
 // [[Rcpp::export]]
 Rcpp::NumericMatrix samploop(Rcpp::NumericMatrix a, Rcpp::NumericVector b) {
 
-    // clone vector and matrix to leave alone
-	int ar = a.nrow();
-	int ac = a.ncol();
+  // clone vector and matrix to leave alone
+  int ar = a.nrow();
+  int ac = a.ncol();
 
-		for (int j = 0; j<ac; j++){
-		  std::shuffle(b.begin(), b.end(), randWrapper);
-			for (int i = 0; i<ar; i++){
-				a(i, j) = b[i];
-			}
-		}
-		return a;
-	}
+  for (int j = 0; j<ac; j++){
+    std::random_shuffle(b.begin(), b.end(), randWrapper);
+    for (int i = 0; i<ar; i++){
+      a(i, j) = b[i];
+    }
+  }
+  return a;
+}
